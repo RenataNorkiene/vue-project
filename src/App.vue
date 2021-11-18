c870063493c49506e3e2ef9f694efc4331118c56
Renata Norkiene <renata.norkiene@iterato.lt>
2021-11-18 16:51:37 +0200

    new vue project setup

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <ul>
+        <learning-resources
+            v-for="res in storedResources"
+            :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        ></learning-resources>
+
+    </ul>
+</template>
+
+<script>
+
+import LearningResources from './components/learning-resources/LearningResources';
+
+export default {
+    name: "App",
+    components: {
+        LearningResources,
+    },
+    data: () => ({
+        storedResources: [
+            {
+            id: 'official-guide',
+            title: 'Official Guide',
+            description: 'The official Vue.js documentation.',
+            link: 'https://vue.js.org'
+            },
+            {
+            id: 'google',
+            title: 'Google',
+            description: 'The official google.com documentation.',
+            link: 'https://google.com'
+            },
+        ],
+    }),
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/learning-resources/LearningResources.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3> {{ title }} </h3>
+                <button>Delete</button>
+            </header>
+        </div>
+        <p> {{ description }}</p>
+        <nav>
+            <a :href="link">View Resource</a>
+        </nav>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "RecourceItem",
+    props: ['title', 'description', 'link'],
+}
+</script>
+
+<style scoped>
+
+</style>
